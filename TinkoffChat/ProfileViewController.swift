f4f12ec601c734422f4a627cd744ec29a7f1a142
iksanov <iksanov@yahoo.com>
2019-02-18 21:03:50 +0300

    start creating profile screen add image files rename ViewController remove ViewController logs

---
+++ TinkoffChat/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TinkoffChat
+//
+//  Created by MacBookPro on 11/02/2019.
+//  Copyright © 2019 Iksanov. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
