ca10b4d50d9e0998a745c68cad02c7d068fe8568
iksanov <iksanov@yahoo.com>
2019-02-25 22:51:16 +0300

    add class and .xib file for conversation cell

---
+++ TinkoffChat/ConversationCell.swift
@@ -0,0 +1,25 @@
+//
+//  ConversationCellView.swift
+//  TinkoffChat
+//
+//  Created by MacBookPro on 25/02/2019.
+//  Copyright © 2019 Iksanov. All rights reserved.
+//
+
+import UIKit
+
+
+class ConversationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
