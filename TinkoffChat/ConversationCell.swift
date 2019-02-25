b8094d456cad0bd8c4b977a661ae8df069ce5af4
iksanov <iksanov@yahoo.com>
2019-02-25 22:57:38 +0300

    add protocol for a cell

--- TinkoffChat/ConversationCell.swift
+++ TinkoffChat/ConversationCell.swift
@@ -8,6 +8,13 @@
 
 import UIKit
 
+protocol ConversationCellConfiguration: class {
+    var name: String? { get set }
+    var message: String? { get set }
+    var date: Date? { get set }
+    var online: Bool { get set }
+    var hasUnreadMessages: Bool { get set }
+}
 
 class ConversationCell: UITableViewCell {
 
