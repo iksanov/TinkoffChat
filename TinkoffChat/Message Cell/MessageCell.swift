d614cd883b751f018b0a3f3719a668ef9f06bd10
iksanov <iksanov@yahoo.com>
2019-02-28 16:54:39 +0300

    add files for message cells

---
+++ TinkoffChat/Message Cell/MessageCell.swift
@@ -0,0 +1,26 @@
+//
+//  MessageCell.swift
+//  TinkoffChat
+//
+//  Created by MacBookPro on 28/02/2019.
+//  Copyright © 2019 Iksanov. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TinkoffChat/Protocols/MessageCellConfiguration.swift
@@ -0,0 +1,13 @@
+//
+//  MessageCellConfiguration.swift
+//  TinkoffChat
+//
+//  Created by MacBookPro on 28/02/2019.
+//  Copyright © 2019 Iksanov. All rights reserved.
+//
+
+import Foundation
+
+protocol MessageCellConfiguration: class {
+    var text: String? { get set }
+}
